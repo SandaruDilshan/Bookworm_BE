3f4566019d572a75eaead2b8340c6401b09a2fb4
Sandaru D Chandrasiri <130263550+SandaruDilshan@users.noreply.github.com>
2025-08-02 04:33:35 +0530

    corn.js added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from './lib/db.js';
 import cors from 'cors';
+import job from "./lib/corn.js";
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json()); 
+job.start(); // Start the cron job
 
 //Routes
 app.use("/api/auth", authRoutes); // Registering the auth routes
---
+++ src/lib/corn.js
@@ -0,0 +1,17 @@
+import corn from 'corn';
+import https from 'https';
+
+const job = new corn.CornJob("*/14****", function () {
+    https
+    .get(process.env.API_URL, (res) => {
+        if (res.statusCode === 200) console.log("GET request send successfully");
+        else console.log("Error in GET request", res.statusCode);
+    })
+    .on("error", (e) => console.error("Error in GET request", e));
+});
+
+export default job;
+
+// MINUITE, HOUR, DAY, MONTH, WEEKDAY
+// *14 * * * *    eevery 14 minutes
+// *30 3 15 * *    every 30 minutes at 3:30 AM on the 15th of every month
