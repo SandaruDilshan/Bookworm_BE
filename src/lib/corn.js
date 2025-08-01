77009fd9626f4c088b642e1136a8b71c2a531c6f
Sandaru D Chandrasiri <130263550+SandaruDilshan@users.noreply.github.com>
2025-08-02 04:38:21 +0530

    corn.js added

--- src/lib/corn.js
+++ src/lib/corn.js
@@ -1,7 +1,7 @@
-import corn from 'corn';
+import cron from 'cron';
 import https from 'https';
 
-const job = new corn.CornJob("*/14****", function () {
+const job = new cron.CronJob("*/14****", function () {
     https
     .get(process.env.API_URL, (res) => {
         if (res.statusCode === 200) console.log("GET request send successfully");
